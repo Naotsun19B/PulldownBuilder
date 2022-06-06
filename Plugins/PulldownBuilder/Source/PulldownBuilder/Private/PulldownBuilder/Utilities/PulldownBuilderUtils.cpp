c1e4ebc90e7380dd625bff5e43da108b80ab4f1a
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2022-06-06 18:35:49 +0900

    Unified file types

--- Plugins/PulldownBuilder/Source/PulldownBuilder/Private/PulldownBuilder/Utilities/PulldownBuilderUtils.cpp
+++ Plugins/PulldownBuilder/Source/PulldownBuilder/Private/PulldownBuilder/Utilities/PulldownBuilderUtils.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright 2021-2022 Naotsun. All Rights Reserved.
+// Copyright 2021-2022 Naotsun. All Rights Reserved.
 
 #include "PulldownBuilder/Utilities/PulldownBuilderUtils.h"
 #include "PulldownStruct/PulldownBuilderGlobals.h"
