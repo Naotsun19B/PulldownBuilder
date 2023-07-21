af5c741897b321aa558597de9a253d37ae312304
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2023-07-21 18:29:07 +0900

    Changed to specify IncludeOrderVersion

--- Source/PulldownBuilderTest.Target.cs
+++ Source/PulldownBuilderTest.Target.cs
@@ -10,6 +10,9 @@ public class PulldownBuilderTestTarget : TargetRules
 #if UE_4_24_OR_LATER
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 #endif
+#if UE_5_2_OR_LATER
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+#endif
 		ExtraModuleNames.AddRange( new string[] { "PulldownBuilderTest" } );
 	}
 }
--- Source/PulldownBuilderTestEditor.Target.cs
+++ Source/PulldownBuilderTestEditor.Target.cs
@@ -10,6 +10,9 @@ public class PulldownBuilderTestEditorTarget : TargetRules
 #if UE_4_24_OR_LATER
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 #endif
+#if UE_5_2_OR_LATER
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+#endif
 		ExtraModuleNames.AddRange( new string[] { "PulldownBuilderTestEditor" } );
 	}
 }
