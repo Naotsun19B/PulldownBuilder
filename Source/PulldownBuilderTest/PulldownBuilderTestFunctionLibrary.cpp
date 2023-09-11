0ca65d8cb7d1f7c6f28b05fc837998681ddf2e3a
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2023-09-11 16:06:51 +0900

    Updated assets so that logs can be checked even in shipping builds

---
+++ Source/PulldownBuilderTest/PulldownBuilderTestFunctionLibrary.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright 2021-2023 Naotsun. All Rights Reserved.
+
+#include "PulldownBuilderTest/PulldownBuilderTestFunctionLibrary.h"
+
+void UPulldownBuilderTestFunctionLibrary::Log(const FText& Message)
+{
+#if WITH_EDITOR
+	static const TArray<FColor, TInlineAllocator<3>> MessageColors =
+	{
+		FColor::Green, FColor::Red, FColor::Cyan
+	};
+	static uint64 Index = 0;
+	GEngine->AddOnScreenDebugMessage(
+		Index,
+		20.f,
+		MessageColors[Index % 3],
+		Message.ToString()
+	);
+	Index++;
+
+	DEFINE_LOG_CATEGORY_STATIC(LogPulldownBuilderTest, Log, All);
+	UE_LOG(LogPulldownBuilderTest, Log, TEXT("%s"), *Message.ToString());
+#else
+	static const FString Filename = (FPaths::ProjectDir() / TEXT("PulldownBuilderTestLog.txt"));
+	static bool bDoOnce = false;
+	TArray<FString> Lines;
+	if (FPaths::FileExists(Filename))
+	{
+		FFileHelper::LoadFileToStringArray(Lines, *Filename);
+		
+		if (!bDoOnce)
+		{
+			Lines.Empty();
+			bDoOnce = true;
+		}
+	}
+	Lines.Add(Message.ToString());
+	FFileHelper::SaveStringArrayToFile(Lines, *Filename, FFileHelper::EEncodingOptions::ForceUTF8);
+#endif
+}
---
+++ Source/PulldownBuilderTest/PulldownBuilderTestFunctionLibrary.h
@@ -0,0 +1,17 @@
+﻿// Copyright 2021-2023 Naotsun. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PulldownBuilderTestFunctionLibrary.generated.h"
+
+UCLASS()
+class PULLDOWNBUILDERTEST_API UPulldownBuilderTestFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = "Pulldown Builder Test", meta = (AutoCreateRefTerm = "Message"))
+	static void Log(const FText& Message);
+};
