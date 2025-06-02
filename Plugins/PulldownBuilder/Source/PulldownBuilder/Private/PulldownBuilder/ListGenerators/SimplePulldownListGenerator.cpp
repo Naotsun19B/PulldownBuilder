5b6d56fbef85aed635cbb9dd0f7d5d8465783443
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2025-06-02 16:08:51 +0900

    UNameArrayPulldownListGenerator was abolished and USimplePulldownListGenerator was added

---
+++ Plugins/PulldownBuilder/Source/PulldownBuilder/Private/PulldownBuilder/ListGenerators/SimplePulldownListGenerator.cpp
@@ -0,0 +1,83 @@
+﻿// Copyright 2021-2025 Naotsun. All Rights Reserved.
+
+#include "PulldownBuilder/ListGenerators/SimplePulldownListGenerator.h"
+#include "PulldownStruct/PulldownBuilderGlobals.h"
+
+#if UE_5_01_OR_LATER
+#include UE_INLINE_GENERATED_CPP_BY_NAME(SimplePulldownListGenerator)
+#endif
+
+#if UE_4_25_OR_LATER
+void USimplePulldownListGenerator::PreEditChange(FProperty* PropertyAboutToChange)
+#else
+void USimplePulldownListGenerator::PreEditChange(UProperty* PropertyAboutToChange)
+#endif
+{
+	Super::PreEditChange(PropertyAboutToChange);
+
+	if (PropertyAboutToChange == nullptr)
+	{
+		return;
+	}
+
+	if (PropertyAboutToChange->GetFName() == GET_MEMBER_NAME_CHECKED(USimplePulldownListGenerator, PulldownRows))
+	{
+		PreChangeSelectedValues.Reset();
+		PreChangeSelectedValues.Reserve(PulldownRows.Num());
+		for (const auto& PulldownRow : PulldownRows)
+		{
+			PreChangeSelectedValues.Add(*PulldownRow.SelectedValue);
+		}
+
+		PreSourceNameArrayModify();
+	}
+}
+
+void USimplePulldownListGenerator::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.MemberProperty == nullptr)
+	{
+		return;
+	}
+
+	if (PropertyChangedEvent.MemberProperty->GetFName() == GET_MEMBER_NAME_CHECKED(USimplePulldownListGenerator, PulldownRows))
+	{
+		TArray<FName> PostChangeSelectedValues;
+		PostChangeSelectedValues.Reserve(PulldownRows.Num());
+		for (const auto& PulldownRow : PulldownRows)
+		{
+			PostChangeSelectedValues.Add(*PulldownRow.SelectedValue);
+		}
+
+		PostSourceNameArrayModify();
+		
+		if (NotifyPulldownRowChanged(PreChangeSelectedValues, PostChangeSelectedValues))
+		{
+			PreChangeSelectedValues.Empty();
+		}
+
+		VerifyDefaultValue();
+	}
+}
+
+FPulldownRows USimplePulldownListGenerator::GetPulldownRows(
+	const TArray<UObject*>& OuterObjects,
+	const FStructContainer& StructInstance
+) const
+{
+	return FPulldownRows(PulldownRows);
+}
+
+TArray<FName> USimplePulldownListGenerator::GetDefaultValueOptions_Implementation() const
+{
+	TArray<FName> DefaultValueOptions;
+	DefaultValueOptions.Reserve(PulldownRows.Num());
+	for (const auto& PulldownRow : PulldownRows)
+	{
+		DefaultValueOptions.Add(*PulldownRow.SelectedValue);
+	}
+	
+	return DefaultValueOptions;
+}
---
+++ Plugins/PulldownBuilder/Source/PulldownBuilder/Public/PulldownBuilder/ListGenerators/SimplePulldownListGenerator.h
@@ -0,0 +1,54 @@
+﻿// Copyright 2021-2025 Naotsun. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PulldownBuilder/ListGenerators/PulldownListGeneratorBase.h"
+#include "PulldownStruct/PulldownBuilderGlobals.h"
+#include "SimplePulldownListGenerator.generated.h"
+
+/**
+ * A generator class that generates a list to be displayed in the pull-down menu from the list of FPulldownRows set directly by the user.
+ */
+UCLASS()
+class PULLDOWNBUILDER_API USimplePulldownListGenerator : public UPulldownListGeneratorBase
+{
+	GENERATED_BODY()
+
+public:
+	// UObject interface.
+#if UE_4_25_OR_LATER
+	virtual void PreEditChange(FProperty* PropertyAboutToChange) override;
+#else
+	virtual void PreEditChange(UProperty* PropertyAboutToChange) override;
+#endif
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	// End of UObject interface.
+	
+	// UPulldownListGeneratorBase interface.
+	virtual FPulldownRows GetPulldownRows(
+		const TArray<UObject*>& OuterObjects,
+		const FStructContainer& StructInstance
+	) const override;
+	// End of UPulldownListGeneratorBase interface.
+
+protected:
+	// Expansion points for implementation in blueprints.
+	UFUNCTION(BlueprintImplementableEvent, Category = "Pulldown", meta = (Tooltip = "Called before a source name array is changed."))
+	void PreSourceNameArrayModify();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Pulldown", meta = (Tooltip = "Called after a source name array has been changed."))
+	void PostSourceNameArrayModify();
+
+	// UPulldownListGeneratorBase interface.
+	virtual TArray<FName> GetDefaultValueOptions_Implementation() const override;
+	// End of UPulldownListGeneratorBase interface.
+	
+protected:
+	// The list of pull-down lines that appear in the list in the pull-down menu.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pulldown")
+	mutable TArray<FPulldownRow> PulldownRows;
+
+	// The cache of selected values before change.
+	UPROPERTY(Transient)
+	TArray<FName> PreChangeSelectedValues;
+};
